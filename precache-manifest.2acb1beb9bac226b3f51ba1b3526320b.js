1fc9e6a84253585d32a6b56d1fde906defb728be
alisagrinko <a.misbakhova@gmail.com>
2020-06-08 23:06:19 -0400

    Updates

---
+++ precache-manifest.2acb1beb9bac226b3f51ba1b3526320b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303f6a279477c18eab23379ee1b043a4",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "c580770d0552b397794b",
+    "url": "/kanban-board/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c580770d0552b397794b",
+    "url": "/kanban-board/static/js/2.89ed166a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-board/static/js/2.89ed166a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bcf275bc33f3ccce33",
+    "url": "/kanban-board/static/js/main.09ba8117.chunk.js"
+  },
+  {
+    "revision": "df7f5190008ec2303916",
+    "url": "/kanban-board/static/js/runtime-main.6be55f1a.js"
+  }
+]);
